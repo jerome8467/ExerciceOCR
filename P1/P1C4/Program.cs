feee14ef81697d43bdd3fab2272c5eb68547e2d4
Jerome Jaegle <jaeglejerome@live.fr>
2026-05-06 01:04:58 +0200

    01h04

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,8 +1,22 @@
 ﻿// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
+using P1C4;
+var monLivre = new Livre;
+
+/*ou
+var monLivre = new Livre
+{
+    Titre = "Titre du livre",
+    Auteur = "Nom de l'auteur",
+    NombreDePages = 100
+};
+*/
 
 // TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
+monLivre.Titre = "Titre du livre";
+monLivre.Auteur = "Nom de l'auteur";
+monLivre.NombreDePages = 100;
 
 // Afficher le contenu des champs de cet objet 
-Console.WriteLine("Le titre du livre est " + monLivre.Titre);
-Console.WriteLine("Son auteur est " + monLivre.Auteur);
-Console.WriteLine("Il compte " + monLivre.NombreDePages);
+Console.WriteLine($"Le titre du livre est {monLivre.Titre}");
+Console.WriteLine($"Son auteur est {monLivre.Auteur}");
+Console.WriteLine($"Il compte {monLivre.NombreDePages}");
--- P3/P3C8/obj/Debug/net8.0/P3C8.AssemblyInfo.cs
+++ P3/P3C8/obj/Debug/net8.0/P3C8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3C8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97dd6311c6ea283894d848512ded6ca71fc3512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0ab106f162991aa8a5bb10ca5e64d36cb49282")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3C8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3C8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
